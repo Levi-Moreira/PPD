64d93f5c293bae8c2aa6f3f14b17e83a6cda9210
Levi Moreira <levi.m.albuquerque@gmail.com>
2017-04-12 17:50:03 -0300

    first attempt at a screen

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Created by ellca on 12/04/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("PPD");
+        frame.setContentPane(new MainWindow().$$$getRootComponent$$$());
+        frame.setVisible(true);
+        frame.setSize(1200,800);
+        frame.setResizable(false);
+
+    }
+}
