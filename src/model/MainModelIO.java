fda0ad296d94f4f78eb05110f42376eda9f86577
Levi Moreira <levi.m.albuquerque@gmail.com>
2017-04-19 09:34:00 -0300

    Merge branch 'new_attempt'
    # Conflicts:
    #	src/Presenter/Presenter.java
    #	src/model/ClientThread.java
    #	src/model/MainModelIO.java
    #	src/model/Server.java

--- src/model/MainModelIO.java
+++ src/model/MainModelIO.java
@@ -1,11 +1,6 @@
 package model;
 
 import Presenter.Presenter;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
-import java.util.List;
 
 /**
  * Created by ellca on 14/04/2017.
@@ -13,54 +8,47 @@ import java.util.List;
 public class MainModelIO {
 
     private ClientThread clientThread;
-    private ClientThread serverThread;
+    private ServerThread serverThread;
 
     private Presenter presenter;
-
-    private String clientName;
 
     public MainModelIO(Presenter presenter) {
         this.presenter = presenter;
     }
 
-    public void startUpClient(String clientName) {
-        this.clientName = clientName;
-        clientThread = new ClientThread(this);
+    public ClientThread startUpClient() {
+        clientThread =  new ClientThread(this);
+        return  clientThread;
+
     }
 
-    public void startUpServer() {
-        serverThread = new ClientThread(this);
+    public ServerThread startUpServer() {
+        serverThread =  new ServerThread(this);
+        return serverThread;
     }
 
     public void clientConnected() {
         presenter.clientConnected();
     }
 
+    public void serverConnected() {
+        presenter.serverConnected();
+    }
 
     public void sendMessage(String text) {
-        Gson gson = new Gson();
-        Type type = new TypeToken<Message>() {
-        }.getType();
-        Message msg = new Message(Message.TYPE_CHAT, clientName, text);
 
-        String json = gson.toJson(msg, type);
+        if (clientThread != null)
+            clientThread.sendMessage(text);
 
-        clientThread.sendMessage(json);
+        if(serverThread!=null)
+            serverThread.sendMessage(text);
     }
 
     public void receivedMessage(String mRcv) {
-        Gson gson = new Gson();
-        Type type = new TypeToken<Message>() {
-        }.getType();
-
-        Message msg = gson.fromJson(mRcv, Message.class);
-
-        if (msg != null)
-            presenter.receivedMessage(msg);
+        presenter.receivedMessage(mRcv);
     }
 
-
-    public void showConnectionError() {
-        presenter.showConnectionError();
+    public void waitingForConnections() {
+        presenter.waitingForConnections();
     }
 }
---
+++ src/model/ServerThread.java
@@ -0,0 +1,107 @@
+package model;
+
+import Presenter.Presenter;
+import view.MainWindow;
+
+import javax.swing.*;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by ellca on 14/04/2017.
+ */
+public class ServerThread extends Thread {
+
+    private boolean keepAlive = true;
+    private String nome;
+
+    private static ArrayList<PrintWriter> clients;
+
+    private static ServerSocket serverSocket = null;
+    private Socket socket = null;
+
+    private static int port = 9090;
+
+    InputStream in;
+    InputStreamReader inr;
+    BufferedReader bfr;
+    Scanner scannerIn;
+
+
+    public ServerThread(Socket socket) {
+        this.socket = socket;
+
+        try {
+            in = socket.getInputStream();
+            //inr = new InputStreamReader(in);
+            //bfr = new BufferedReader(inr);
+            scannerIn = new Scanner(in);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void run() {
+        try {
+
+            String msg;
+            OutputStream ou = socket.getOutputStream();
+            PrintWriter prw = new PrintWriter(ou, true);
+
+            clients.add(prw);
+            nome = msg = scannerIn.nextLine();
+
+            while (keepAlive && msg != null) {
+                if (scannerIn.hasNextLine()) {
+                    msg = scannerIn.nextLine();
+                    sendToAll(prw, msg);
+                    System.out.println(msg);
+                }
+
+            }
+        } catch (Exception e) {
+            System.out.print("ServerRun: ");
+            System.out.println(e);
+        }
+    }
+
+    public void setKeepAlive(boolean keepAlive) {
+        this.keepAlive = keepAlive;
+    }
+
+
+    public void sendToAll(PrintWriter prwSaida, String msg) throws IOException {
+        PrintWriter bwS;
+
+        for (PrintWriter pw : clients) {
+            bwS = (PrintWriter) pw;
+            if (!(prwSaida == bwS)) {
+                pw.println(msg);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            serverSocket = new ServerSocket(port);
+            clients = new ArrayList<PrintWriter>();
+
+            while (clients.size() < 2) {
+                System.out.println("Esperando por conecões");
+                Socket socket = serverSocket.accept();
+                System.out.println("Cliente conectado");
+                Thread t = new ServerThread(socket);
+                t.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
