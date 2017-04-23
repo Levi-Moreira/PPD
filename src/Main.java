4a950ce5e82aae1cd32be51ec74abb31169ccfc7
Levi Moreira <levi.m.albuquerque@gmail.com>
2017-04-23 00:53:05 -0300

    improve UI

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,16 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"); //Windows Look and feel
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
 
         JFrame frame = new JFrame("PPD");
         ClientWindow myGui = new ClientWindow(frame);
         frame.setContentPane(myGui.$$$getRootComponent$$$());
-        frame.setSize(1000, 900);
+        frame.setSize(1300, 1000);
         frame.setResizable(false);
         frame.setVisible(true);
         myGui.startUp();
