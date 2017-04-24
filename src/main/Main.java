e570706a9b4a7501c57f270b1a0af199ee64519e
Levi Moreira <levi.m.albuquerque@gmail.com>
2017-04-23 23:46:53 -0300

    comment the main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,23 +5,31 @@ import view.ClientWindow;
 import javax.swing.*;
 
 /**
- * Created by ellca on 12/04/2017.
+ * Main entry point for the client
  */
 public class Main {
     public static void main(String[] args) {
 
+        //change the look and feel to a better experience
         try {
             UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel"); //Windows Look and feel
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
             e.printStackTrace();
         }
 
+        //start up the frame that will hold the main window
         JFrame frame = new JFrame("PPD");
+
+        //start up the main window
         ClientWindow myGui = new ClientWindow(frame);
         frame.setContentPane(myGui.$$$getRootComponent$$$());
         frame.setSize(1300, 1000);
         //frame.setResizable(false);
+
+        //show it
         frame.setVisible(true);
+
+        //start up the user interface
         myGui.startUp();
 
 
